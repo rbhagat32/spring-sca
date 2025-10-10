1dc3e26cf71a9f12c3166ebdd1d692e5ce49812c
rbhagat32 <raghavbhagat32@gmail.com>
2025-10-11 01:10:18 +0530

    horizontal scaling

--- backend/src/main/java/com/rbhagat32/auth/backend/security/JwtFilter.java
+++ backend/src/main/java/com/rbhagat32/auth/backend/security/JwtFilter.java
@@ -34,7 +34,7 @@ public class JwtFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain filterChain) throws ServletException, IOException {
         try {
-            // log.info("JWT Filter: Incoming request on {}", request.getRequestURI());
+            log.info("JWT Filter: Incoming request on {}", request.getRequestURI());
 
             String token = null;
             String userId = null;
