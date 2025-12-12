466d21d39f4ed287ac895ff6e0c211f4002027aa
rbhagat32 <raghavbhagat32@gmail.com>
2025-12-13 00:24:36 +0530

    fix toaster getting unmounted on pageloader

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,15 @@
-import { Routing } from "@/utils/routing";
 import { PageLoader } from "@/components/custom/page-loader";
-import { Toaster } from "sonner";
 import { useUser } from "@/context/user-provider";
+import { Routing } from "@/utils/routing";
+import { Toaster } from "sonner";
 
 export function App() {
   const { loading, user } = useUser();
 
-  return loading ? (
-    <PageLoader fullScreen={true} />
-  ) : (
+  return (
     <>
+      {loading ? <PageLoader /> : <Routing isLoggedIn={!!user} />}
       <Toaster richColors position="top-center" duration={5000} />
-      <Routing isLoggedIn={!!user} />
     </>
   );
 }
