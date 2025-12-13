e8a14efcb8d918be2100f77dc93dd3a816b92de3
rbhagat32 <raghavbhagat32@gmail.com>
2025-12-13 08:13:57 +0530

    remove section tag bcos HomePage component has it

--- frontend/src/routes/_protected/route.tsx
+++ frontend/src/routes/_protected/route.tsx
@@ -16,10 +16,7 @@ function RouteComponent() {
     <SocketProvider>
       <main className="relative mx-auto min-h-screen max-w-screen-sm overflow-y-hidden rounded-sm border bg-zinc-900 p-4">
         <NavBar />
-
-        <section>
-          <Outlet />
-        </section>
+        <Outlet />
       </main>
     </SocketProvider>
   );
